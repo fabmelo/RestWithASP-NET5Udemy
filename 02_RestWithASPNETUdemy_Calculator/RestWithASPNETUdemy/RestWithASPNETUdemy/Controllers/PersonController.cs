0d62473161696824bd5395bb138c365e1fa65d8f
Fabiano Melo Moreira <fabmelo@gmail.com>
2021-03-06 18:25:29 -0300

    Criada a classe person

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{operation}/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string operation, string firstNumber, string secondNumber)
+        {         
+            return BadRequest("Invalid input");
+        }
+
+     
+      
+    }
+}
