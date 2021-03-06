37ccf7817601b0237641e10b43aa92af489f78de
Fabiano Melo Moreira <fabmelo@gmail.com>
2021-03-06 18:33:45 -0300

    Criada a interface para Person

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(IPersonService person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
